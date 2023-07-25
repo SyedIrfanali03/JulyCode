99216c672426af44cb2ac0152c283129fdd5dce3
GopiKrishna19977 <greenstaskmay@gmail.com>
2023-07-25 13:57:01 +0530

    added few lines

---
+++ src/org/test/TestNg.java
@@ -0,0 +1,19 @@
+package org.test;
+
+import org.testng.annotations.Test;
+
+public class TestNg {
+
+	@Test
+	public void syed() 
+	{
+	System.out.println("tc1");	
+	}
+	
+	@Test
+	private void irfan() {
+		System.out.println("India");
+
+	}
+	
+}
